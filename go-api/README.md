f6b45356c69cd2d93633623d11c9072941fa766f
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2016-03-21 22:10:35 +0900

    Set up minimum go_server and dockerfile

--- go-api/README.md
+++ go-api/README.md
@@ -1,9 +1,16 @@
 # Go-lang based API Server
 
-Go言語で
+Go言語でJSONを返すAPIサーバーを作る場合
 
 ## 基本設計
 
 - HTTP Handler: [negroni](https://github.com/codegangsta/negroni)
 - JSON rendering: [render](https://github.com/unrolled/render)
 - Routing: [httptreemux](https://github.com/dimfeld/httptreemux)
+
+
+## Dockerで使える形式でコンパイルする
+
+```
+GOOS=linux GOARCH=amd64 go build -o bin/server_linux_amd64 server.go
+```
---
+++ go-api/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "net/http"
+
+    "github.com/codegangsta/negroni"
+    "github.com/dimfeld/httptreemux"
+    "github.com/unrolled/render"
+)
+
+func main() {
+    ren := render.New()
+    router := httptreemux.New()
+
+    router.GET("/", func(w http.ResponseWriter, req *http.Request, params map[string]string) {
+        ren.JSON(w, http.StatusOK, map[string]string{"message": "hello go server!"})
+    })
+    router.GET("/:name", func(w http.ResponseWriter, req *http.Request, params map[string]string) {
+        ren.JSON(w, http.StatusOK, map[string]string{"message": "hello " + params["name"] + "!" })
+    })
+    n := negroni.Classic()
+    n.UseHandler(router)
+    n.Run(":8000")
+}
