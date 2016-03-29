86f561da280fabafd4d60d2d03b3eeb24e474af4
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2016-03-29 23:05:06 +0900

    Set up root path by minimum change

--- rails-api/app/controllers/application_controller.rb
+++ rails-api/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def index
+    render json: { message: "hello rails server!" }
+  end
 end
--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
+  root to: "application#index"
 end
